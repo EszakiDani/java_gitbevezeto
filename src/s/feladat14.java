758ce4d8885f54a47610ec40b2ef84a5fa9ca7ee
Eszaki Daniel <info@petrik.hu>
2022-09-13 16:04:02 +0200

    y

--- src/s/feladat14.java
+++ src/s/feladat14.java
@@ -2,6 +2,8 @@ package s;
 
 import com.sun.source.tree.SynchronizedTree;
 
+import java.sql.SQLOutput;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class feladat14 {
@@ -18,6 +20,40 @@ public class feladat14 {
             System.out.println("\td - Összehasonlítani egy másik szöveggel");
             System.out.println("\te - A szöveg egy részét kiirni");
             System.out.println("\tf - Kilépni");
+            System.out.println("Adja meg a menüpontot: ");
+            menuPont = sc.nextLine().toLowerCase();
+            switch (menuPont){
+                case "a":
+                    System.out.println(szoveg.toUpperCase());
+                    break;
+                case "b":
+                    System.out.println(szoveg.toLowerCase());
+                    break;
+                case "c":
+                    System.out.printf("A szöveg hossza %d karakter\n",szoveg.length());
+                    break;
+                case "d":
+                    System.out.print ("Kérem adja meg a másik szöveg");
+                    String szoveg2 = sc.nextLine();
+                    int osszehasonlitas= szoveg.compareTo(szoveg2);
+                    if (osszehasonlitas<0){
+                        System.out.println("Az első szöveg van előrébb az ABC-ben");
+                    }else if (osszehasonlitas> 0){
+                        System.out.println("A második szöveg van előrébb az ABC-ben");
+                    }else{
+                        System.out.println("A két szöveg megegyezik");
+                    }
+                    break;
+                case "e":
+                    break;
+                case "f":
+                    System.out.println("Viszlát!");
+                    break;
+                default:
+                    System.out.println("Rossz lehetőséget adott meg, kérem a menüpont közül válasszon!");
+                    break;
+
+            }
 
         }
     }
