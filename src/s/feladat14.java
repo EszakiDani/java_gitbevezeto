e877a2d7ddc9779ef88eaf8b887779ea40705db2
Eszaki Daniel <info@petrik.hu>
2022-09-13 15:37:35 +0200

    y

---
+++ src/s/feladat14.java
@@ -0,0 +1,11 @@
+package s;
+
+import java.util.Scanner;
+
+public class feladat14 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Kérem:" );
+        String szoveg =sc.nextLine();
+    }
+}
